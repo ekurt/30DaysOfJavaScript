1be7c0398a2c70606f2feef762088e76eba89ab1
Emre Kurt <emrekurtmf@gmail.com>
2022-03-04 18:08:49 +0300

    day 18 level 2 completed

--- day-18/app.js
+++ day-18/app.js
@@ -22,3 +22,18 @@ fetch(countriesAPI)
         cgc("L1 Q1", data.map(item => [item.name, item.capital, item.languages.map(i => i.name), item.population, item.area]))
     })
     .catch(error => console.error(error))
+
+/*** L2 ***/
+
+// Q1
+const fetchData = async () => {
+    try {
+      const response = await fetch(catsAPI)
+      const cats = await response.json()
+      return cats.map(item => item.name)
+    } catch (err) {
+      console.error(err)
+    }
+}
+
+cgc("L2 Q1", fetchData())
