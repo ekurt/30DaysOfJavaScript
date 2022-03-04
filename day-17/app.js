5ce5c9bdb5f240f7bee671f09c56a8add1ddb624
Emre Kurt <emrekurtmf@gmail.com>
2022-03-04 13:30:19 +0300

    day 17 completed

--- day-17/app.js
+++ day-17/app.js
@@ -35,3 +35,61 @@ const studentJSON = JSON.stringify(student, undefined, 4)
 localStorage.setItem('student', studentJSON)
 
 cgc("L2 Q1", localStorage)
+
+/*** L3 ***/
+
+// Q1
+const personAccount = {
+    firstName: "Emre",
+    lastName: "Kurt",
+    incomes: [0],
+    expenses: [0],
+    totalIncome: function() {
+        let totalIncome = 0
+
+        for (let income of this.incomes) {
+            totalIncome += income
+        }
+
+        return totalIncome
+    },
+    totalExpense: function() {
+        let totalExpense = 0
+
+        for (let expense of this.expenses) {
+            totalExpense += expense
+        }
+
+        return totalExpense
+    },
+    accountInfo: function() {
+        return `First Name: ${this.firstName} \nLast Name: ${this.lastName} \nTotal Income: ${this.totalIncome()} \nTotal Expense: ${this.totalExpense()} \nAccount Balance: ${this.accountBalance()}`
+    },
+    addIncome: function(amount) {
+        this.incomes.push(amount)
+        return `$${amount} added to incomes`
+    },
+    addExpense: function(amount) {
+        this.expenses.push(amount)
+        return `$${amount} added to expenses`
+    },
+    accountBalance: function() {
+        let incomes = this.totalIncome(),
+            expenses = this.totalExpense(),
+            balance = incomes - expenses
+
+        return balance                
+    }
+}
+
+personAccount.addIncome(50)
+personAccount.addIncome(500)
+personAccount.addIncome(230)
+personAccount.addExpense(100)
+personAccount.addExpense(115)
+personAccount.addExpense(15)
+
+const personAccountJSON = JSON.stringify(personAccount, undefined, 4)
+localStorage.setItem('personAccount', personAccountJSON)
+
+cgc("L3 Q1", localStorage)
