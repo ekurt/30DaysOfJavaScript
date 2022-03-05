eb4afc283f0f69b1c7d9d5d9657f0ce84f83a70b
Emre Kurt <emrekurtmf@gmail.com>
2022-03-05 11:51:32 +0300

    day 18 completed

--- day-18/app.js
+++ day-18/app.js
@@ -37,3 +37,44 @@ const fetchData = async () => {
 }
 
 cgc("L2 Q1", fetchData())
+
+/*** L3 ***/
+
+// Q1
+fetch(catsAPI)
+    .then(response => response.json())
+    .then(data => {
+        cgc("L3 Q1", data.map(item => {
+            return item.weight.metric.split('-').reduce((acc, cur) => acc + Number(cur), 0) / 2
+        }))
+    })
+    .catch(error => console.error(error))
+
+// Q2
+fetch(countriesAPI)
+    .then(response => response.json())
+    .then(data => {
+        const arr =  data.map(item => [item.name, item.area]).sort((a, b) => b[1] - a[1])
+        const array = []
+
+        for (let i = 0; i < 10; i++) {
+            array.push(arr[i])
+        }
+
+        cgc("L3 Q2", array)
+    })
+    .catch(error => console.error(error))
+
+// Q3
+fetch(countriesAPI)
+    .then(response => response.json())
+    .then(data => {
+        
+        const allLangs = data.map(item => item.languages.map(item => item.name)).join(',').split(',').sort((a, b) => a.localeCompare(b))
+
+        const langs = new Set(allLangs)
+
+        cgc("L3 Q3", langs, langs.size)
+
+    })
+    .catch(error => console.error(error))
