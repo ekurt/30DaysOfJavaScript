f2d625cf6f60e70cff7318c46d01f3cc9d453732
Emre Kurt <emrekurtmf@gmail.com>
2022-03-06 14:21:31 +0300

    day 21 level 2 completed

--- day-21/app.js
+++ day-21/app.js
@@ -27,3 +27,25 @@ p4.innerText = "Lorem ipsum dolar sit amed"
 p1.setAttribute('class', 'new-class')
 allParagraphs[1].setAttribute('class', 'new-class')
 p3.classList.add('title', 'new-class')
+
+/*** L2 ***/
+
+// Q1
+p1.style.color = 'red'
+p2.style.backgroundColor = 'blue'
+p3.style.fontSize = '24px'
+p4.style.border = '1px solid #ccc'
+
+// Q2
+allParagraphs.forEach((title, i) => {
+    if (i % 2 === 0) {
+      title.style.color = 'green'
+    } else {
+      title.style.color = 'red'
+    }
+})
+
+// Q3
+p1.innerHTML = '<h1>whats up</h1>'
+p2.className = 'no-way'
+p3.id = 'good-job'
